d03a0e0966cd54289800f01ce0b1e297013d451b
Maxi Perez <55637107+Durzdor@users.noreply.github.com>
2023-12-17 22:31:31 -0300

    BookUI Settings tab

--- Assets/Code/Scripts/Managers/GameStateManager.cs
+++ Assets/Code/Scripts/Managers/GameStateManager.cs
@@ -1,4 +1,8 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Cursor = UnityEngine.Cursor;
 
 public class GameStateManager : MonoBehaviour
 {
@@ -7,15 +11,39 @@ public class GameStateManager : MonoBehaviour
     // player rotation with mouse
     // player attack vfx
     // Parecen ser todos los eventos de mouse que si se siguen actualizando
+    public static bool GameIsPaused;
     
+    [SerializeField] private Button pauseButton;
     
     public void InactiveState()
     {
         Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.lockState = CursorLockMode.None;
+        pauseButton.gameObject.SetActive(false);
     }
 
     public void ActiveState()
     {
         Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Confined;
+        pauseButton.gameObject.SetActive(true);
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GoToDesktop()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
     }
 }
